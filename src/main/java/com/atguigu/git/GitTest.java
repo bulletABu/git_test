80c842129a1440a3e78cfd95cf21de1ddbb380e4
bullet <2513465779@qq.com>
2022-11-09 21:22:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author YSH
+ * @create 2022-11-09 21:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
